c331e0b689546ccac525c18d971e44fc4e35e8e4
Boland <Boland.Michael@mayo.edu>
2017-10-20 15:46:46 -0500

    Added roxygen2 documentation, changed package requirements

--- R/ost.R
+++ R/ost.R
@@ -1,3 +1,16 @@
+#' Generate Output for a One Sample t-Test
+#'
+#'This function will generate the output for a one sample t test.
+#'
+#' @param x a formula Dependent Variable ~ 1 or a data frame column containing the Dependent Variable
+#' @param y optional, a data frame when data frame is not referenced when a formula is passed to x
+#' @param tails an integer indicating whether the test is one-tailed '1' or two-tailed '2'
+#' @param mu a double representing the value you want to comare to, the default for this parameter is 0.0
+#'
+#' @return A list of output for reporting $analysis_type, $results, $descriptive_statistics
+#' @export
+#'
+#' @examples ost <- ost(idt_data$scones, tails = 1, mu = 44)
 ost <- function(x, y = NULL, tails = 2, mu = 0.0) {
   form <- is.formula(x)
 
