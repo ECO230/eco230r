b345ef509152f0cf6a2c5b96e4e06d276dd621dd
Boland <Boland.Michael@mayo.edu>
2020-05-10 23:10:21 -0500

    helper functions yet again

--- R/final_file.R
+++ R/final_file.R
@@ -6,5 +6,6 @@ final_file <- function(){
 library(digest)
     truth <- read_csv('pbgc_clean.csv')[[1,1]] == paste(digest(Sys.getenv('RSTUDIO_USER_IDENTITY_DISPLAY'),algo='sha256'),1,sep='')[1]
   test <- paste('File Hash Match:',as.character(truth))
-  return(test)
+  hash <- as.character(Sys.getenv('R_FILE_HASH'))
+  return(c(test,hash))
 }
