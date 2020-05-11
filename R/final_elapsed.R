ab4ff8e67b5832fd3e556a3618f2c9220d13feaa
Boland <Boland.Michael@mayo.edu>
2020-05-10 22:37:42 -0500

    Update helper function

--- R/final_elapsed.R
+++ R/final_elapsed.R
@@ -3,11 +3,19 @@
 #'This function will tell how much time has passed since loading the file
 #' @export
 final_elapsed <- function(){
-library(lubridate)
   st <- ymd_hms(Sys.getenv('R_FINAL_FILE_TIME'))
   en <- ymd_hms(Sys.time())
   tint <- st %--% en
-  elps <- as.duration(tint)
+  elps <- as.character(as.duration(tint))
 
-  return(elps)
+  now <-as.character(Sys.time())
+  est <- as.character(Sys.getenv('R_FINAL_FILE_TIME'))
+  fud <- (1000*60*60*21)
+  frt <- max(read_csv('pbgc_clean.csv')$Data_Accessed) + fud
+
+  fl <- paste('File Accessed:',frt)
+  ft <- paste('Environment Started:',est)
+  fn <- paste('Current Time:',now)
+  fe <- paste('Elapsed Time:',elps)
+  return(c(fl,ft,fn,fe))
 }
